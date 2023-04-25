5bab1a6b201934a4cb46d6fcde57c21c26da876d
Dirk Arnez <smalldirkalex@gmail.com>
2023-04-25 22:03:57 +0800

    - upload files

---
+++ create.cmd
@@ -0,0 +1,5 @@
+@echo off
+set PATH=%USERPROFILE%\Downloads\flutter_windows_3.7.12-stable\flutter\bin;%SystemRoot%\System32
+
+flutter create hello_world
+pause
---
+++ run.cmd
@@ -0,0 +1,6 @@
+@echo off
+set PATH=%USERPROFILE%\Downloads\flutter_windows_3.7.12-stable\flutter\bin;%SystemRoot%\System32
+
+cd hello_world &&^
+flutter run
+pause
